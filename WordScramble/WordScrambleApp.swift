70730f6dba1402f9e28a017891fc76bdd0a98753
Miguel Quezada <mig.aquezada@gmail.com>
2022-05-22 01:14:13 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Miguel Quezada on 22-05-22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
