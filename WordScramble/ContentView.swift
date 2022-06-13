841593138f9299eaed219d1a2ef7ca21df5dad2e
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-13 01:13:21 -0400

    add accessibility

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -35,6 +35,9 @@ struct ContentView: View {
                                 Image(systemName: "\(word.count).circle")
                                 Text(word)
                             }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
